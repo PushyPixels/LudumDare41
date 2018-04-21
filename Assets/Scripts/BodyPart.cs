57e428c8f747485080b9121a69cc239d61fba528
Maximilian Maroe <drakfyre@gmail.com>
2018-04-21 03:21:46 -0700

    Basic BodyPart script

---
+++ Assets/Scripts/BodyPart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+    public enum Slot
+    {
+        Head,
+        LeftArmLower,
+        LeftArmUpper,
+        LeftLegLower,
+        LeftLegUpper,
+        RightArmLower,
+        RightArmUpper,
+        RightLegLower,
+        RightLegUpper,
+        TorsoLower,
+        TorsoUpper
+    }
+    public Slot slot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
